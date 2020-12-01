9dcef33ac360510ca274681dee6c3eb7fa21516d
samir ardjouni <samir.ardjouni@gmail.com>
2020-12-01 21:04:07 +0100

    creation classe CommandeCat

---
+++ src/poo/cda/commande/CommandeCat.java
@@ -0,0 +1,20 @@
+package poo.cda.commande;
+
+import java.util.List;
+
+public class CommandeCat extends Commande {
+
+	protected CommandeCat(int pId, String pDescription, List<T> args) {
+		super(pId, pDescription, args);
+
+	}
+
+	@Override
+	public boolean executer() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	// cc it's mee
+
+}
